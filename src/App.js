c7e540e369a4a911e07e6aa483b4cb8560376022
harish-m-xelp <harish@xelpmoc.in>
2019-11-07 10:45:15 +0530

    news api integration

--- src/App.js
+++ src/App.js
@@ -2,13 +2,49 @@ import React from "react";
 import "./App.css";
 import Newscard from "../src/components/newsCard";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Todays's Hot News!!!</h1>
-      <Newscard />
-    </div>
-  );
-}
+import Load from "../src/components/loader";
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newsData: "",
+      error: "",
+      loader: true
+    };
+  }
+
+  componentWillMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch(
+      `https://newsapi.org/v2/top-headlines?country=us&apiKey=5e854054b73f484eb3e3a9e836088064`
+    )
+      .then(data => data.json())
+      .then(data =>
+        this.setState({
+          newsData: data,
+          loader: false
+        })
+      )
+      .catch(e => this.setState({ error: e }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {!this.state.loader ? (
+          <div>
+            <h1>Todays's Hot News!!!</h1>
+            <Newscard newsFeed={this.state.newsData} />
+          </div>
+        ) : (
+          <Load />
+        )}
+      </div>
+    );
+  }
+}
 export default App;
--- src/components/newsCard.js
+++ src/components/newsCard.js
@@ -2,67 +2,32 @@ import React from "react";
 import Media from "react-media";
 import styles from "./newCard.module.css";
 
-function Newscard() {
+function Newscard(props) {
+  const { articles } = props.newsFeed;
+
   return (
-    <React.Fragment>
-      <Media query="(max-width:800px)">
-        <div className={styles.container}>
-          <img
-            className={styles.image}
-            src="https://techcrunch.com/wp-content/uploads/2019/10/GettyImages-1059655038.jpg?w=600"
-          />
-          <div className={styles.header}>
-            <h1>Southeast Asian real estate portal 99.co</h1>
+    <div>
+      {articles &&
+        articles.map(feed => {
+          const { author, urlToImage, title, content } = feed;
 
-            <p>
-              Southeast Asian real estate portal 99.co has agreed to form a
-              joint venture with iProperty. As part of the deal, iProperty owner
-              REA Group will invest $8 million of working capital into the
-              venture, expected to be finalized by the second quarter of 2020.
-              99.co and REA Group, a real estate-focused digital advertising
-              conglomerate that is listed on the Australian Securities Exchange
-              (ASX), say that the JV immediately makes 99.co the market leader
-              in Indonesia and positions it to take the top spot in Singapore,
-              as well. The deal also makes 99.co a more formidable rival to
-              PropertyGuru. Backed by TPG Capital and KKR, PropertyGuru is
-              expected to raise up to AUD $380.2 million (about USD $255.9
-              million) in an IPO on the ASX this month. The joint venture is
-              expected to be finalized by the second quarter of 2020 and 99.co
-              will assume full control of REA Group brands iProperty.com.sg in
-              Singapore and Rumah123.com in Indonesia.
-            </p>
-            <div className={styles.author}>---By Haarry</div>
-          </div>
-        </div>
-      </Media>
-      <Media query="(min-width:1024px)">
-        <div className={styles.deskContainer}>
-          <img
-            className={styles.deskimage}
-            src="https://techcrunch.com/wp-content/uploads/2019/10/GettyImages-1059655038.jpg?w=600"
-          />
-          <div className={styles.header}>
-            <h2>Southeast Asian real estate portal 99.co</h2>
+          return (
+            <Media query="(min-width:1024px)">
+              <div className={styles.deskContainer}>
+                <img className={styles.deskimage} src={urlToImage} />
+                <div className={styles.header}>
+                  <h2>{title}</h2>
 
-            <p>
-              Southeast Asian real estate portal 99.co has agreed to form a
-              joint venture with iProperty. As part of the deal, iProperty owner
-              REA Group will invest $8 million of working capital into the
-              venture, expected to be finalized by the second quarter of 2020.
-              99.co and REA Group, a real estate-focused digital advertising
-              conglomerate that is listed on the Australian Securities Exchange
-              (ASX), say that the JV immediately makes 99.co the market leader
-              in Indonesia and positions it to take the top spot in Singapore,
-              as well. The deal also makes 99.co a more formidable rival to
-              PropertyGuru. Backed by TPG Capital and KKR, PropertyGuru is
-              expected to raise up to AUD $380.2 million (about USD $255.9
-              million) in an IPO on the ASX this month.
-            </p>
-            <div className={styles.author}>---By Haarry</div>
-          </div>
-        </div>
-      </Media>
-    </React.Fragment>
+                  <p>{content}</p>
+                  <div className={styles.author}>{author}</div>
+                </div>
+                <br />
+                <br />
+              </div>
+            </Media>
+          );
+        })}
+    </div>
   );
 }
 
